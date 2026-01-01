275189da233b2b0ea1cb74273bd0a54e2fa444c1
Justin Toon <1739079+overneath42@users.noreply.github.com>
2026-01-01 12:32:37 -0500

    Install container queries plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import containerQueries from "@tailwindcss/container-queries";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./*.html", "./src/**/*.{js,ts,html}"],
@@ -52,5 +54,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [containerQueries],
 };
